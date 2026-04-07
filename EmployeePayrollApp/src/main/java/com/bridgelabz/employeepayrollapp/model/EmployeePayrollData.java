85f1585f5fdd383e0daef213b78d0ddd90a675f6
mehul1110 <mehulshra1110@gmail.com>
2026-04-07 12:31:53 +0530

    UC15 - Successfully migrated Employee Payroll endpoints from static collections to Spring Data JPA MySQL.

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -2,24 +2,61 @@ package com.bridgelabz.employeepayrollapp.model;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+
 import lombok.Data;
 
+@Entity
+@Table(name="employee_payroll")
 public @Data class EmployeePayrollData {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="employee_id")
     private int employeeId;
+
+    @Column(name="name")
     private String name;
+
+    @Column(name="salary")
     private long salary;
+
+    @Column(name="gender")
     private String gender;
+
+    @Column(name="start_date")
     private LocalDate startDate;
+
+    @Column(name="note")
     private String note;
+
+    @Column(name="profile_pic")
     private String profilePic;
+
+    @ElementCollection
+    @CollectionTable(name="employee_department", joinColumns = @JoinColumn(name="id"))
+    @Column(name="department")
     private List<String> departments;
 
     public EmployeePayrollData() {
     }
 
-    public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
-        this.employeeId = empId;
+    public EmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+        this.updateEmployeePayrollData(empPayrollDTO);
+    }
+    
+    public void updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
         this.name = empPayrollDTO.name;
         this.salary = empPayrollDTO.salary;
         this.gender = empPayrollDTO.gender;
--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -1,50 +1,49 @@
 package com.bridgelabz.employeepayrollapp.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.exception.EmployeePayrollException;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+import com.bridgelabz.employeepayrollapp.repository.EmployeePayrollRepository;
 
 @Service
 public class EmployeePayrollService implements IEmployeePayrollService {
 
-    private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+    @Autowired
+    private EmployeePayrollRepository employeeRepository;
 
     @Override
     public List<EmployeePayrollData> getEmployeePayrollData() {
-        return employeePayrollList;
+        return employeeRepository.findAll();
     }
 
     @Override
     public EmployeePayrollData getEmployeePayrollDataById(int empId) {
-        return employeePayrollList.stream()
-                .filter(empData -> empData.getEmployeeId() == empId)
-                .findFirst()
+        return employeeRepository.findById(empId)
                 .orElseThrow(() -> new EmployeePayrollException(
                         "Employee with id " + empId + " not found"));
     }
 
     @Override
     public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
-        EmployeePayrollData empData = new EmployeePayrollData(employeePayrollList.size() + 1, empPayrollDTO);
-        employeePayrollList.add(empData);
-        return empData;
+        EmployeePayrollData empData = new EmployeePayrollData(empPayrollDTO);
+        return employeeRepository.save(empData);
     }
 
     @Override
     public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
-        empData.setName(empPayrollDTO.name);
-        empData.setSalary(empPayrollDTO.salary);
-        return empData;
+        empData.updateEmployeePayrollData(empPayrollDTO);
+        return employeeRepository.save(empData);
     }
 
     @Override
     public void deleteEmployeePayrollData(int empId) {
-        employeePayrollList.removeIf(empData -> empData.getEmployeeId() == empId);
+        EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
+        employeeRepository.delete(empData);
     }
 }
