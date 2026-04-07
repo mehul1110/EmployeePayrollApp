9e197c51efd1500feee4ef17375f22b52e9f64ec
mehul1110 <mehulshra1110@gmail.com>
2026-04-07 12:07:51 +0530

    UC13 - Expand Data Models with gender, startDate, note, profilePic, department

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,22 +1,27 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.ToString;
 
-public class EmployeePayrollDTO {
+public @ToString class EmployeePayrollDTO {
 
     @NotBlank(message = "Employee name is required")
     @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     public String name;
 
     public long salary;
+    public String gender;
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    public List<String> department;
 
     public EmployeePayrollDTO(String name, long salary) {
         this.name = name;
         this.salary = salary;
-    }
-
-    public String toString() {
-        return "name=" + name + ":salary=" + salary;
     }
 }
--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,11 +1,19 @@
 package com.bridgelabz.employeepayrollapp.model;
 
+import java.time.LocalDate;
+import java.util.List;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import lombok.Data;
 
-public class EmployeePayrollData {
+public @Data class EmployeePayrollData {
     private int employeeId;
     private String name;
     private long salary;
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> departments;
 
     public EmployeePayrollData() {
     }
@@ -14,29 +22,10 @@ public class EmployeePayrollData {
         this.employeeId = empId;
         this.name = empPayrollDTO.name;
         this.salary = empPayrollDTO.salary;
-    }
-
-    public int getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
+        this.gender = empPayrollDTO.gender;
+        this.startDate = empPayrollDTO.startDate;
+        this.note = empPayrollDTO.note;
+        this.profilePic = empPayrollDTO.profilePic;
+        this.departments = empPayrollDTO.department;
     }
 }
